fb06e09fd7c61837f56eaa9cfb9f63a4dc28b6da
amir <amirtaki623@gmail.com>
2025-08-03 22:10:09 +0330

    javaScript part 2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 const inputEl = document.getElementById("input")
 const infoTextEl = document.getElementById("info-text")
+const meaningContainerEl = document.getElementById("meaning-container")
+const title = document.getElementById("title")
