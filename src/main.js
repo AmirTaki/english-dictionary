568f447bd0594d013647148ba1bab2ba8e64035e
amir <amirtaki623@gmail.com>
2025-08-03 22:12:15 +0330

    javaScript part 4

--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ const meaningContainerEl = document.getElementById("meaning-container")
 const title = document.getElementById("title")
 const meaningEl = document.getElementById("meaning")
 const audioEl = document.getElementById("audio")
+
+
+const fetachAPI = async (word) => {
+    try {
+        
+    }
+    catch(error){
+
+    }
+}
